5271dd53a4c53d341bb561493764c9da3ce87fce
Takai <tkbunbury@gmail.com>
2024-05-20 15:15:33 +0100

    completed-onboarding-screen

--- constants/images.js
+++ constants/images.js
@@ -5,5 +5,8 @@ import path from "../assets/images/path.png";
 import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
+import cards_2 from "../assets/images/cards_2.png";
+import logo_2 from "../assets/images/logo_2.png";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+
+export default { profile, thumbnail, cards, cards_2, path, logo, logo_2, logoSmall, empty };
