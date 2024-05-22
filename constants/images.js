8489a36e8079f46cfe6eafb88753126d5c723ca7
Takai <tkbunbury@gmail.com>
2024-05-22 19:30:53 +0100

    First part of homescreen implemented

--- constants/images.js
+++ constants/images.js
@@ -7,6 +7,7 @@ import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
 import cards_2 from "../assets/images/cards_2.png";
 import logo_2 from "../assets/images/logo_2.png";
+import logoSmall_2 from "../assets/images/logo-small-2.png"
 
 
-export default { profile, thumbnail, cards, cards_2, path, logo, logo_2, logoSmall, empty };
+export default { profile, thumbnail, cards, cards_2, path, logo, logo_2, logoSmall, logoSmall_2, empty };
